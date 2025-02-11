6ff748d120755cbd9bba426fb17969b2f6c387bf
Paramesh <Paramesh.developer@gmail.com>
2025-02-11 07:42:46 +0530

    Constructor Injection

--- app/src/main/java/com/hiltexample/Test.kt
+++ app/src/main/java/com/hiltexample/Test.kt
@@ -6,12 +6,23 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    var main = runBlocking {
-        var job : Job =launch {
-           delay(1000)
-            println("This is launch ")
-        }
-        println("This is Outer runBlocking")
+    data class Student(var name: String, val age: Int)
 
+    val arraylist: ArrayList<Student> = ArrayList<Student>()
+    arraylist.add(Student("Mallangi", 14))
+    arraylist.add(Student("Parameswar", 23))
+    arraylist.add(Student("Parameswar", 23))
+    arraylist.add(Student("Reddy", 19))
+
+
+    val list = arraylist.filter { student: Student -> student.age < 18 }
+    println(list.size)
+
+    val list1 = arraylist.map { student: Student -> student.name + "  Reddy" }
+    println(list1.size)
+
+    list1.forEach {
+        println(it)
     }
+    val list2 = arraylist.remove(Student("Reddy",19))
 }
